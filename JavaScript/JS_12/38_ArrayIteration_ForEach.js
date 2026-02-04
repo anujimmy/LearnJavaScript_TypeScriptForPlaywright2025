c08f7f2b4f97e69ce1101a4be7244d2ca245d41f
anujimmy <anuannajimmy@gmail.com>
2026-02-05 00:24:51 +0100

    updated 38_ArrayIteration_forEach.js

--- JavaScript/JS_12/38_ArrayIteration_ForEach.js
+++ JavaScript/JS_12/38_ArrayIteration_ForEach.js
@@ -1,22 +1,22 @@
-let vowels =['a','e','i','o','u'];
+let vowels = ['a', 'e', 'i', 'o', 'u'];
 
 // for Each
 
-vowels.forEach(ele =>{ 
+vowels.forEach(ele => {
     console.log('vowel : ' + ele);
 });
 
 // in the forEach() - We are passing an arrow function.
 
-let num = [1,3,5,30,5,6];
+let num = [1, 3, 5, 30, 5, 6];
 
 
 let evenFlag = num.forEach(iNum => {
-    if(iNum % 2 === 0){
+    if (iNum % 2 === 0) {
         console.log(iNum + ' is even');
         //break ; // syntax error
         // return true; // no change in output
-    }else{
+    } else {
         console.log(iNum + ' is odd');
     }
 });
@@ -26,7 +26,7 @@ console.log('======some======'); //returns boolean value
 
 let Output = num.some(iNum => {
     console.log(iNum);
-    if(iNum % 10 === 0 && iNum % 2 === 0){
+    if (iNum % 10 === 0 && iNum % 2 === 0) {
         console.log(iNum + ' is divisible by 10');
         return true;
         // break; // break cannot be used   
@@ -35,3 +35,16 @@ let Output = num.some(iNum => {
 });
 
 console.log(Output);
+console.log('==================');
+let product = ['apple_macbook','dell_XPS','lenovo_ideapad','apple_iPad','dell_monitor','apple_iphone'];
+
+let arrApple = [];
+
+let getAppleProd = product.forEach(iProd => {
+    console.log(iProd);
+    if(iProd.includes('apple')){
+        arrApple.push(iProd);
+    }
+});
+
+console.log(arrApple);
